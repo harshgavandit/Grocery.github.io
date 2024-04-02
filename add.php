a46e5311b7ee618fadc16bc6e6bd6162d5c301f0
harshgavandit <128570357+harshgavandit@users.noreply.github.com>
2024-04-03 00:30:19 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,101 @@
+<?php
+
+@include 'db.php';
+
+if(isset($_POST['add_product'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+ 
+  
+   if(empty($product_name) || empty($product_price) ){
+      $message[] = 'please fill out all';
+   }else{
+      $insert = "INSERT INTO products(name, price ) VALUES('$product_name', '$product_price')";
+      $upload = mysqli_query($conn,$insert);
+      if($upload){
+
+         $message[] = 'new product added successfully';
+      }else{
+         $message[] = 'could not add the product';
+      }
+   }
+
+};
+
+if(isset($_GET['delete'])){
+   $id = $_GET['delete'];
+   mysqli_query($conn, "DELETE FROM products WHERE id = $id");
+   header('location:add.php');
+};
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Farmer's Page </title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/add.css">
+
+</head>
+<body>
+
+<?php
+
+?>
+   
+<div class="container">
+
+   <div class="admin-product-form-container">
+
+      <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+         <h3>add a new product</h3>
+         <input type="text" placeholder="enter product name" name="product_name" class="box">
+         <input type="number" placeholder="enter product price" name="product_price" class="box">
+
+         <input type="submit" class="btn" name="add_product" value="add product">
+      </form>
+
+   </div>
+
+   <?php
+
+   $select = mysqli_query($conn, "SELECT * FROM products");
+   
+   ?>
+   <div class="product-display">
+      <table class="product-display-table">
+         <thead>
+         <tr>
+            <th>product name</th>
+            <th>product price</th>
+            <th>action</th>
+         </tr>
+         </thead>
+         <?php while($row = mysqli_fetch_assoc($select)){ ?>
+         <tr>
+            
+            <td><?php echo $row['name']; ?></td>
+            <td>$<?php echo $row['price']; ?>/-</td>
+            <td>
+               <a href="add.php?delete=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-trash"></i> delete </a>
+            </td>
+         </tr>
+      <?php } ?>
+      </table>
+   </div>
+
+</div>
+
+
+</body>
+</html>
---
+++ cookies.js
@@ -0,0 +1,9 @@
+
+
+function setCookie(cookieName, cookieValue, expirationDays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+  }
+  
